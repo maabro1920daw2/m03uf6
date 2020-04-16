b6e5a7d787306e346aae5678ce00f06352f4be7a
maabro1920daw2 <maabro1920daw2@gmail.com>
2020-04-16 19:05:13 +0200

    javafx escena login y registro acabada

--- src/controllers/InicioController.java
+++ src/controllers/InicioController.java
@@ -5,12 +5,21 @@
  */
 package controllers;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
 
 /**
  * FXML Controller class
@@ -20,12 +29,50 @@ import javafx.scene.image.ImageView;
 public class InicioController implements Initializable {
     @FXML
     private ImageView imgLogo;
+    @FXML
+    private AnchorPane content_area;
+    @FXML
+    private TextField loginName;
+    @FXML
+    private TextField loginPass;
+    @FXML
+    private Button btnLogin;
+    @FXML
+    private Button btnRegister;
+    @FXML
+    private TextField nombreUsuario;
+    @FXML
+    private TextField passwordUsuario;
+    @FXML
+    private ComboBox<?> tipoUsuario;
+    @FXML
+    private Button btnRegistro;
+    @FXML
+    private Button btnVolver;
+    @FXML
+    private Pane panelInicio;
+    @FXML
+    private Pane panelRegistro;
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         imgLogo.setImage(new Image ("file:/"+System.getProperty("user.dir").replace("\\", "/")+"/assets/img/logo_375x210px.png"));
-    }    
+    }
+
+    @FXML
+    private void irRegistro(MouseEvent event) {
+        if(event.getSource().equals(btnRegister)){
+            panelRegistro.toFront();
+        }
+    }
+
+    @FXML
+    private void irLogin(MouseEvent event) {
+        if(event.getSource().equals(btnVolver)){
+            panelInicio.toFront();
+        }
+    }
     
 }
